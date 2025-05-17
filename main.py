1152e480f9e7adbe2832e1d6421c967556c1913d
Khuzaim Hamid <khuzaimahamid17@gmail.com>
2025-05-17 03:59:00 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+temprature = [10, 20, 10, 14, 15, 9, 13]
+sum_temprature = sum(temprature)
+count_temprature = len(temprature)
+avg_temprature = sum_temprature / avg_temprature
+print(f"average temprature: {avg_temprature}")
